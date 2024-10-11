6ee8ae352194e70ff88319b9296fd13135c29d11
Newton Ni <nwtnni@gmail.com>
2024-10-10 21:09:52 -0500

    Set up nix infrastructure and build

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs; {
+        devShells.default = mkShell.override { stdenv = clangStdenv; } {
+          nativeBuildInputs = [
+            autoconf
+            automake
+            db
+            fmt
+            jemalloc
+            libaio
+            libxcrypt
+            libz
+            numactl
+            openssl
+          ];
+        };
+      }
+    );
+}
